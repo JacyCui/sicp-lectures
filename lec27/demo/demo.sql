2110ff04a8700ddd1091248c276adb30c1577e9d
JacyCui <jiacaicui@163.com>
2022-08-25 19:31:08 +0800

    lec27-demo

---
+++ lec27/demo/demo.sql
@@ -0,0 +1,10 @@
+.open --new
+
+create table animals as
+  select "dog" as kind, 4 as legs, 20 as weight union
+  select "cat"        , 4        , 10           union
+  select "ferret"     , 4        , 10           union
+  select "parrot"     , 2        , 6            union
+  select "penguin"    , 2        , 10           union
+  select "t-rex"      , 2        , 12000;
+  
